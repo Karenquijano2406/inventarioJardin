f21af36f58612bfd3eca46371972be289688ba01
Karenquijano2406 <karenquijano2406@gmail.com>
2025-01-31 21:38:38 -0600

    tabla productos agregados recientemente

--- vistas/modulos/inicio/cajasSuperiores.php
+++ vistas/modulos/inicio/cajasSuperiores.php
@@ -1,92 +1,129 @@
-
-
-        
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-aqua">
+<div class="row" style="margin: 10px;">
+    <!-- Cajas Superiores -->
+    <div class="col-lg-3 col-xs-6">
+        <div class="small-box bg-aqua">
             <div class="inner">
-              <h3>150</h3>
-
-              <p>New Orders</p>
+                <h3>150</h3>
+                <p>New Orders</p>
             </div>
             <div class="icon">
-              <i class="ion ion-bag"></i>
+                <i class="ion ion-bag"></i>
             </div>
             <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
         </div>
-        <!-- ./col -->
-
-
-
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-green">
-            <div class="inner">
-              <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-              <p>Bounce Rate</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-stats-bars"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-
-
-
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-yellow">
-            <div class="inner">
-              <h3>44</h3>
-
-              <p>User Registrations</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-person-add"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-
-
-
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-red">
-            <div class="inner">
-              <h3>65</h3>
-
-              <p>Unique Visitors</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-pie-graph"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-      </div>
-      <!-- /.row -->
-
-
-     
-
-
-    
-
     </div>
-
-
-
-
-
-
-
-
-
     
+    <div class="col-lg-3 col-xs-6">
+        <div class="small-box bg-green">
+            <div class="inner">
+                <h3>53<sup style="font-size: 20px">%</sup></h3>
+                <p>Bounce Rate</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    
+    <div class="col-lg-3 col-xs-6">
+        <div class="small-box bg-yellow">
+            <div class="inner">
+                <h3>44</h3>
+                <p>User Registrations</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-person-add"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    
+    <div class="col-lg-3 col-xs-6">
+        <div class="small-box bg-red">
+            <div class="inner">
+                <h3>65</h3>
+                <p>Unique Visitors</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+</div>
+
+
+
+<!-- Tabla debajo de las cajas -->
+<div class="box box-info mt-3" style="margin: 20px; padding: 15px;">
+    <div class="box-header with-border">
+        <h3 class="box-title">Productos Agregados Recientemente</h3>
+        <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+        </div>
+    </div>
+    <div class="box-body" style="padding: 10px;">
+        <div class="table-responsive">
+            <table class="table no-margin" style="margin: 10px; padding: 10px;">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nombre</th>
+                        <th>Precio Venta</th>
+                        <th>Stock</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><a href="#">OR9842</a></td>
+                        <td>Call of Duty IV</td>
+                        <td><span class="label label-success">Shipped</span></td>
+                        <td>90%</td>
+                    </tr>
+                    <tr>
+                        <td><a href="#">OR1848</a></td>
+                        <td>Samsung Smart TV</td>
+                        <td><span class="label label-warning">Pending</span></td>
+                        <td>85%</td>
+                    </tr>
+                    <tr>
+                        <td><a href="#">OR7429</a></td>
+                        <td>iPhone 6 Plus</td>
+                        <td><span class="label label-danger">Delivered</span></td>
+                        <td>78%</td>
+                    </tr>
+                    <tr>
+                        <td><a href="#">OR7429</a></td>
+                        <td>Samsung Smart TV</td>
+                        <td><span class="label label-info">Processing</span></td>
+                        <td>82%</td>
+                    </tr>
+                    <tr>
+                        <td><a href="#">OR1848</a></td>
+                        <td>Samsung Smart TV</td>
+                        <td><span class="label label-warning">Pending</span></td>
+                        <td>88%</td>
+                    </tr>
+                    <tr>
+                        <td><a href="#">OR7429</a></td>
+                        <td>iPhone 6 Plus</td>
+                        <td><span class="label label-danger">Delivered</span></td>
+                        <td>74%</td>
+                    </tr>
+                    <tr>
+                        <td><a href="#">OR9842</a></td>
+                        <td>Call of Duty IV</td>
+                        <td><span class="label label-success">Shipped</span></td>
+                        <td>92%</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="box-footer clearfix">
+        <a href="#" class="btn btn-sm btn-info btn-flat pull-left">Productos</a>
+        <a href="#" class="btn btn-sm btn-default btn-flat pull-right">Ver todos los productos</a>
+    </div>
+</div>
